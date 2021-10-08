3869e3207b4e3d06492b972c55225c49e4e0ffaf
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-10-08 10:56:49 +0700

    mod mainactivity

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -13,7 +13,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+
         btnMoveActivity.setOnClickListener(this)
+        btnMoveWithDataActivity.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -21,6 +24,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
+                startActivity(moveWithDataIntent)
             }
         }
     }
